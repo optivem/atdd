c55b7cbe000e918946db9e6028404dedb13805a4
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-08 14:02:32 +0200

    Initial commit

---
+++ src/main/java/com/optivem/atdd/AtddApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.atdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optivem/atdd/AtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivem.atdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
