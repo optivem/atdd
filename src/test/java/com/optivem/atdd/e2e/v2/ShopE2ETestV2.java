564e4f6b9913a626ea64875f661353ee4fb90c2f
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-08 16:17:52 +0200

    E2E V2 - Test

---
+++ src/test/java/com/optivem/atdd/e2e/v2/ShopE2ETestV2.java
@@ -0,0 +1,44 @@
+package com.optivem.atdd.e2e.v2;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ShopE2ETestV2 {
+
+    @LocalServerPort
+    private int port;
+
+    private WebDriver seleniumDriver;
+    private String baseUrl;
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        seleniumDriver = new ChromeDriver(options);
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterEach
+    void tearDown() {
+        seleniumDriver.quit();
+    }
+
+    @Test
+    void shouldCompletePurchaseSuccessfully() {
+        ShopPage shopPage = new ShopPage(seleniumDriver);
+        shopPage.open(baseUrl);
+        shopPage.enterSku("8");
+        shopPage.enterQuantity("5");
+        shopPage.placeOrder();
+
+        String text = shopPage.getConfirmationMessage();
+        assertThat(text).matches("Success! Total price is \\$\\d+(\\.\\d{2})?");
+    }
+}
