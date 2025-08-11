24f87c37d3a8093027069037a426238ffbdefd09
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-11 16:44:09 +0200

    Acceptance Tests - WireMock - Removed Spring Boot WireMock because it appeats to be for embedded only

--- src/test/java/com/optivem/atdd/acceptancetests/ShopAccTest.java
+++ src/test/java/com/optivem/atdd/acceptancetests/ShopAccTest.java
@@ -12,27 +12,21 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.wiremock.spring.EnableWireMock;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("acc")
-@EnableWireMock
 public class ShopAccTest {
 
     @LocalServerPort
     private int port;
 
     @Value("${erp.url}")
-    private String wireMockUrl;
+    private String erpUrl;
 
     private WebDriver seleniumDriver;
     private String baseUrl;
 
-//    private WireMockServer erpWireMockStub;
-
-//    private WebClient erpStubWebClient;
+    private WebClient webClient;
 
     @BeforeEach
     void setUp() {
@@ -41,31 +35,26 @@ public class ShopAccTest {
         seleniumDriver = new ChromeDriver(options);
         baseUrl = "http://localhost:" + port;
 
-//        erpStubWebClient = WebClient.create("http://localhost:8089");
-
-
-//        erpWireMockStub = new WireMockServer(8089);
-//        erpWireMockStub.start();
-//        configureFor("localhost", 8089);
+        webClient = WebClient.create(erpUrl);
     }
 
     @AfterEach
     void tearDown() {
         seleniumDriver.quit();
-
-//        erpWireMockStub.stop();
     }
 
     @Test
     public void shouldCompletePurchaseSuccessfully() {
-//        stubFor(get(urlPathEqualTo("/products/8"))
-////                .withQueryParam("sku", equalTo("APPLE1001"))
-//                .willReturn(aResponse()
-//                        .withStatus(200)
-//                        .withHeader("Content-Type", "application/json")
-//                        .withBody("{\"price\": 2.50}")));
-//
-        WebClient webClient = WebClient.create("http://localhost:8089");
+        stubOut();
+
+        var shop = new ShopDsl(new UiDriver(seleniumDriver, baseUrl + "/shop"));
+
+        shop.placeOrder("sku: 8", "quantity: 5");
+        shop.assertConfirmation("message: Success! Total price is $12.50");
+    }
+
+    private void stubOut() {
+
 
         String stubJson = """
 {
@@ -92,11 +81,5 @@ public class ShopAccTest {
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
-
-
-        var shop = new ShopDsl(new UiDriver(seleniumDriver, baseUrl + "/shop"));
-
-        shop.placeOrder("sku: 8", "quantity: 5");
-        shop.assertConfirmation("message: Success! Total price is $12.50");
     }
 }
