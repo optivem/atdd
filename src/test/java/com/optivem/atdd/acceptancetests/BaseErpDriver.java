ac6a41cd2e0234f144dd787dcf5013fbb5aa2971
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-11 16:36:19 +0200

    Acceptance Tests - Stub Driver - Initial (blank)

---
+++ src/test/java/com/optivem/atdd/acceptancetests/BaseErpDriver.java
@@ -0,0 +1,22 @@
+package com.optivem.atdd.acceptancetests;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class BaseErpDriver {
+//    protected final WebClient webClient;
+//
+//    protected BaseErpDriver(WebClient webClient) {
+//        this.webClient = webClient;
+//    }
+//
+//    public ProductResponse getProduct(String sku) {
+//        return webClient.get()
+//                .uri(uriBuilder -> uriBuilder
+//                        .path("/products")
+//                        .queryParam("sku", sku)
+//                        .build())
+//                .retrieve()
+//                .bodyToMono(ProductResponse.class)
+//                .block();
+//    }
+}
---
+++ src/test/java/com/optivem/atdd/acceptancetests/ErpStubDriver.java
@@ -0,0 +1,18 @@
+package com.optivem.atdd.acceptancetests;
+
+public class ErpStubDriver extends BaseErpDriver {
+//    private final WireMock erpWireMockStub;
+//
+//    public ErpStubDriver(WireMock erpWireMockStub) {
+//        this.erpWireMockStub = erpWireMockStub;
+//    }
+//
+//    public void setupProduct(String sku, String price) {
+//        erpWireMockStub.stubFor(get(urlPathEqualTo("/products"))
+//                .withQueryParam("sku", equalTo(sku))
+//                .willReturn(aResponse()
+//                        .withStatus(200)
+//                        .withHeader("Content-Type", "application/json")
+//                        .withBody("{\"price\": " + price + "}")));
+//    }
+}
