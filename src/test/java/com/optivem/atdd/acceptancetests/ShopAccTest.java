a0b37617f89be87193c687ca45bd03770923a6f0
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-11 15:07:45 +0200

    Acceptance Tests - WireMock - Added Docker Compose

--- src/test/java/com/optivem/atdd/acceptancetests/ShopAccTest.java
+++ src/test/java/com/optivem/atdd/acceptancetests/ShopAccTest.java
@@ -1,6 +1,5 @@
 package com.optivem.atdd.acceptancetests;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +9,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("e2e")
@@ -23,7 +20,7 @@ public class ShopAccTest {
     private WebDriver seleniumDriver;
     private String baseUrl;
 
-    private WireMockServer erpWireMockStub;
+//    private WireMockServer erpWireMockStub;
 
     @BeforeEach
     void setUp() {
