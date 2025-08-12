590bdeb61bee210fe1400514019a0dd3f0bb1001
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-12 15:32:47 +0200

    WireMock - Java Library - Instance methods

--- src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/external/erp/ErpStubDriver.java
+++ src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/external/erp/ErpStubDriver.java
@@ -10,63 +10,19 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 
 public class ErpStubDriver extends BaseErpDriver {
 
-//    private final WireMock erpWireMockStub;
+    private final WireMock erpWireMockStub;
 
     public ErpStubDriver(WebClient webClient) {
         super(webClient);
-        // this.erpWireMockStub = new WireMock("localhost", 8089);
-        // this.erpWireMockStub = new WireMock(8089);
-        // this.erpWireMockStub = null;
-        WireMock.configureFor("localhost", 8089);
+        this.erpWireMockStub = new WireMock("localhost", 8089);
     }
 
     public void setupProduct(String sku, String price) {
         var url = "/products/" + sku;
-        WireMock.stubFor(get(urlPathEqualTo(url))
+        erpWireMockStub.register(get(urlPathEqualTo(url))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody("{\"price\": " + price + "}")));
-
-//        erpWireMockStub.stubFor(get(urlPathEqualTo(url))
-//                .willReturn(aResponse()
-//                        .withStatus(200)
-//                        .withHeader("Content-Type", "application/json")
-//                        .withBody("{\"price\": " + price + "}")));
     }
-
-
-//    public ErpStubDriver(WebClient webClient) {
-//        super(webClient);
-//    }
-//
-//    public void setupProduct(String sku, String price) {
-//        // TODO: VJ: Use variables
-//
-//        String stubJson = String.format("""
-//        {
-//          "request": {
-//            "method": "GET",
-//            "urlPath": "/products/%s"
-//          },
-//          "response": {
-//            "status": 200,
-//            "headers": {
-//              "Content-Type": "application/json"
-//            },
-//            "jsonBody": {
-//              "price": %s
-//            }
-//          }
-//        }
-//        """, sku, price);
-//
-//        webClient.post()
-//                .uri("/__admin/mappings")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .bodyValue(stubJson)
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-//    }
 }
