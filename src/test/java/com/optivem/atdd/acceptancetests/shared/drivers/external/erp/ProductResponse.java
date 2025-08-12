a174c1db5292d7875ec9cc04580843216f372b2b
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-12 13:32:54 +0200

    Added Lombok

--- src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/external/erp/ProductResponse.java
+++ src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/external/erp/ProductResponse.java
@@ -1,22 +1,9 @@
 package com.optivem.atdd.acceptancetests.shared.drivers.external.erp;
 
+import lombok.Data;
+
+@Data
 public class ProductResponse {
     private String sku;
     private String price;
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
 }
