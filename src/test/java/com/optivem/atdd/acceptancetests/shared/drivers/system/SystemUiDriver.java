65a72fd7047f5007dcc6b53474dd327c04c90c5c
Valentina Jemuovic <valentina.jemuovic@optivem.com>
2025-08-14 11:38:31 +0200

    SystemUiDriver - Refactoring - Introduced Page

--- src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/system/SystemUiDriver.java
+++ src/test/java/com/optivem/atdd/acceptancetests/shared/drivers/system/SystemUiDriver.java
@@ -13,7 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SystemUiDriver implements SystemDriver {
     private final WebDriver driver;
     private final String shopUrl;
-    private final WebDriverWait wait;
+
+
+    private final ShopPage shopPage;
 
     private static final Pattern ORDER_CONFIRMATION_PATTERN = Pattern.compile(
             "Success! Order has been created with Order Number ([\\w-]+) and Total Price \\$(\\d+(?:\\.\\d{2})?)"
@@ -26,7 +28,9 @@ public class SystemUiDriver implements SystemDriver {
         this.driver = driver;
         this.shopUrl = shopUrl;
 
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        this.shopPage = new ShopPage(driver);
     }
 
     @Override
@@ -36,39 +40,75 @@ public class SystemUiDriver implements SystemDriver {
 
     @Override
     public void submitOrder(String orderNumber, String sku, String quantity) {
-        driver.findElement(By.cssSelector("[aria-label='SKU']")).sendKeys(sku);
-        driver.findElement(By.cssSelector("[aria-label='Quantity']")).sendKeys(quantity);
-        driver.findElement(By.cssSelector("[aria-label='Place Order']")).click();
+        shopPage.enterSku(sku);
+        shopPage.enterQuantity(quantity);
+        shopPage.clickPlaceOrder();
     }
 
     @Override
     public void confirmOrderNumberGenerated(String orderNumber) {
-        var confirmationElement = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[role='alert']"))
-        );
-
-        var confirmation = confirmationElement.getText();
-
-        var matcher = ORDER_CONFIRMATION_PATTERN
-                .matcher(confirmation);
-
-        assertThat(matcher.find()).isTrue();
-        assertThat(matcher.group(ORDER_NUMBER_GROUP)).isNotEmpty();
+        var confirmation = shopPage.getConfirmationMessage();
+        var externalOrderNumber = getOrderNumberFromConfirmation(confirmation);
+        assertThat(externalOrderNumber).isNotEmpty();
     }
 
     @Override
     public void confirmOrderTotalPrice(String orderNumber, String expectedTotalPrice) {
-        var confirmationElement = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[role='alert']"))
-        );
+        var confirmation = shopPage.getConfirmationMessage();
+        var totalPrice = getTotalPriceFromConfirmation(confirmation);
+        assertThat(totalPrice).isEqualTo(expectedTotalPrice);
+    }
 
-        var confirmation = confirmationElement.getText();
+    private static String getOrderNumberFromConfirmation(String confirmation) {
+        var matcher = ORDER_CONFIRMATION_PATTERN.matcher(confirmation);
+        if (matcher.find()) {
+            return matcher.group(ORDER_NUMBER_GROUP);
+        }
+        return null;
+    }
 
-        var matcher = ORDER_CONFIRMATION_PATTERN
-                .matcher(confirmation);
+    private static String getTotalPriceFromConfirmation(String confirmation) {
+        var matcher = ORDER_CONFIRMATION_PATTERN.matcher(confirmation);
+        if (matcher.find()) {
+            return matcher.group(TOTAL_PRICE_GROUP);
+        }
+        return null;
+    }
 
-        assertThat(matcher.find()).isTrue();
-        assertThat(matcher.group(TOTAL_PRICE_GROUP)).isEqualTo(expectedTotalPrice);
+    private class ShopPage {
+        private static final String SKU_INPUT = "[aria-label='SKU']";
+        private static final String QUANTITY_INPUT = "[aria-label='Quantity']";
+        private static final String PLACE_ORDER_BUTTON = "[aria-label='Place Order']";
+        private static final String ALERT_ROLE = "[role='alert']";
+
+        private final WebDriver driver;
+
+        private final WebDriverWait wait;
+
+        public ShopPage(WebDriver driver) {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        }
+
+        public void enterSku(String sku) {
+            driver.findElement(By.cssSelector(SKU_INPUT)).sendKeys(sku);
+        }
+
+        public void enterQuantity(String quantity) {
+            driver.findElement(By.cssSelector(QUANTITY_INPUT)).sendKeys(quantity);
+        }
+
+        public void clickPlaceOrder() {
+            driver.findElement(By.cssSelector(PLACE_ORDER_BUTTON)).click();
+        }
+
+        public String getConfirmationMessage() {
+            var confirmationElement = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ALERT_ROLE))
+            );
+
+            return confirmationElement.getText();
+        }
     }
 
 
